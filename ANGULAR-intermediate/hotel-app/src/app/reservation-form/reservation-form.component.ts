3fb52a7a18cc0de087dbd9b8451ced6bd6beaf8b
Walter Larrea <walterlarrea@live.com>
2024-05-07 18:03:28 -0300

    add: angular basics, section 2 - vid #50

---
+++ ANGULAR-intermediate/hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
+
+@Component({
+  selector: 'app-reservation-form',
+  templateUrl: './reservation-form.component.html',
+  styleUrls: ['./reservation-form.component.css'],
+})
+export class ReservationFormComponent implements OnInit {
+  reservationForm: FormGroup = new FormGroup({});
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private reservationService: ReservationService
+  ) {}
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      let newReservation: Reservation = this.reservationForm.value;
+      this.reservationService.addReservation(newReservation);
+    }
+  }
+}
