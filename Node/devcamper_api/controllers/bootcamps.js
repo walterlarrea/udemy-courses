d8ffd05da7e85e6e08e21fb0b83b9845658b0879
Walter Larrea <walterlarrea@live.com>
2024-05-13 15:04:55 -0300

    addÑ nodejs, section 5

--- Node/devcamper_api/controllers/bootcamps.js
+++ Node/devcamper_api/controllers/bootcamps.js
@@ -1,94 +1,74 @@
-const ErrorResponse = require('../utils/errorResponse.js');
-const Bootcamp = require('../models/Bootcamp.js');
+const ErrorResponse = require("../utils/errorResponse.js");
+const Bootcamp = require("../models/Bootcamp.js");
+const asyncHandler = require("../middleware/async.js");
 
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
 // @access    Public
-exports.getBootcamps = async (req, res, next) => {
-	try {
-		const bootcamps = await Bootcamp.find();
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
 
-		res
-			.status(200)
-			.json({ success: true, count: bootcamps.length, data: bootcamps });
-	} catch (error) {
-		next(error);
-	}
-};
+  res
+    .status(200)
+    .json({ success: true, count: bootcamps.length, data: bootcamps });
+});
 
 // @desc      Get single bootcamps
 // @route     GET /api/v1/bootcamps/:id
 // @access    Public
-exports.getBootcamp = async (req, res, next) => {
-	try {
-		const bootcamp = await Bootcamp.findById(req.params.id);
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
-		if (!bootcamp) {
-			return next(
-				new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-			);
-		}
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
 
-		res.status(200).json({ success: true, data: bootcamp });
-	} catch (error) {
-		// res.status(400).json({ success: false });
-		next(error);
-	}
-};
+  res.status(200).json({ success: true, data: bootcamp });
+});
 
 // @desc      Create new bootcamp
 // @route     POST /api/v1/bootcamps
 // @access    Public
-exports.createBootcamps = async (req, res, next) => {
-	try {
-		const bootcamp = await Bootcamp.create(req.body);
+exports.createBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
 
-		res.status(201).json({
-			success: true,
-			data: bootcamp,
-		});
-	} catch (error) {
-		next(error);
-	}
-};
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
+});
 
 // @desc      Update bootcamp
 // @route     PUT /api/v1/bootcamps/:id
 // @access    Public
-exports.updateBootcamps = async (req, res, next) => {
-	try {
-		const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-			new: true,
-			runValidators: true,
-		});
+exports.updateBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-		if (!bootcamp) {
-			return next(
-				new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-			);
-		}
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
 
-		res.status(200).json({ success: true, data: bootcamp });
-	} catch (error) {
-		next(error);
-	}
-};
+  res.status(200).json({ success: true, data: bootcamp });
+});
 
 // @desc      Delete bootcamp
 // @route     DELETE /api/v1/bootcamps/:id
 // @access    Public
-exports.deleteBootcamps = async (req, res, next) => {
-	try {
-		const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+exports.deleteBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
-		if (!bootcamp) {
-			return next(
-				new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-			);
-		}
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
 
-		res.status(200).json({ success: true, data: {} });
-	} catch (error) {
-		next(error);
-	}
-};
+  res.status(200).json({ success: true, data: {} });
+});
