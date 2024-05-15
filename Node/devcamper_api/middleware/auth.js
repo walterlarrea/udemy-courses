addfac6426f6b6ab0ef39e2818b52beba07ae2b2
Walter Larrea <walterlarrea@live.com>
2024-05-15 18:16:50 -0300

    add: nodejs, section 10

--- Node/devcamper_api/middleware/auth.js
+++ Node/devcamper_api/middleware/auth.js
@@ -10,16 +10,16 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    // Set token from Bearer in header
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.token) {
+    // Set token from cookie
+    token = req.cookies.token;
   }
-
-  // else if(req.cookies.token) {
-  //   token = req.cookies.token
-  // }
 
   // Make sure token exists
   if (!token) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 
   try {
@@ -31,7 +31,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
     next();
   } catch (error) {
     console.error(error);
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
 
